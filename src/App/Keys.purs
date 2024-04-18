a34af186e83ebf89361ef98914548f5265e82fb5
Ulric Wilfred <shaman.sir@gmail.com>
2024-04-19 01:28:14 +0200

    draft for the key combos

--- src/App/Keys.purs
+++ src/App/Keys.purs
@@ -3,4 +3,47 @@ module App.Keys where
 import Prelude
 
 
-foo = 42
+import Data.Array.NonEmpty (NonEmptyArray)
+import Data.Array.NonEmpty as NE
+
+
+data Modifier
+    = Shift
+    | Command
+    | Option
+
+
+data Key
+    = Alpha Char
+    | Num Int
+    | Special Int String
+
+
+data Combo
+    = Combo (NonEmptyArray Key) String
+    | ModCombo Modifier Combo
+    | Sequence Combo Combo
+
+
+shift :: Modifier
+shift = Shift
+
+
+cmd :: Modifier
+cmd = Command
+
+
+opt :: Modifier
+opt = Option
+
+
+char :: Char -> String -> Combo
+char c = Combo $ NE.singleton $ Alpha c
+
+
+code :: Int -> String -> String -> Combo
+code n symbol = Combo $ NE.singleton $ Special n symbol
+
+
+mod :: Modifier -> Combo -> Combo
+mod = ModCombo
--- src/App/State.purs
+++ src/App/State.purs
@@ -2,9 +2,16 @@ module App.State where
 
 import Prelude
 
+import Data.Array.NonEmpty ((:))
+import Data.Array.NonEmpty as NE
+
 
 import Data.Text.Format.Org.Types (OrgFile)
 import Data.Text.Format.Org.Construct (empty) as Org
+
+import App.Keys (Combo(..), Key(..), Modifier(..))
+import App.Keys (shift, opt, cmd) as Mod
+import App.Keys (char, code, mod) as Combo
 
 
 data Context
@@ -50,3 +57,45 @@ init =
     , file : Org.empty
     , chars : ""
     }
+
+
+suggestions :: Context -> Array Combo
+suggestions = const []
+
+
+combo = Combo
+
+
+-- TODO: group combos
+
+
+topLevel :: Array Combo
+topLevel =
+    [ Combo.char 'n' "add title" -- Change title if it exists
+    , Combo.char 'v' "add property"
+    , Combo.char 'p' "add paragraph"
+    , Combo.char 'h' "add heading" -- same as task / item
+    , Combo.char 'k' "add task"
+    , Combo.char 'i' "add item"
+    , Combo.char 'a' "agenda"
+    ]
+
+
+headingSelected :: Array Combo
+headingSelected =
+    [ Combo.char 'e' "edit heading"
+    , Combo.code 45 "->" "level deep"
+    , Combo.code 45 "<-" "level higher" -- If not at the top
+    , Combo.code 43 "" "move down"
+    , Combo.code 41 "" "move up"
+    , Combo.mod Mod.shift $ Combo.code 45 "->" "cycle todo level fwd"
+    , Combo.mod Mod.shift $ Combo.code 42 "<-" "cycle todo level back"
+    , Combo.mod Mod.shift $ Combo.code 45 "" "priority up"
+    , Combo.mod Mod.shift $ Combo.code 42 "" "priority down"
+    , Combo.char 's' "schedule"
+    , Combo.char 'd' "deadline"
+    , Combo.char 'r' "repeat"
+    , Combo.char 't' "timestamp"
+    -- TODO: sequence to edit todo manually
+    -- TODO: sequence to edit priority manually
+    ]
