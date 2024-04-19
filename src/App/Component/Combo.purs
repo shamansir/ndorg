f779da8118089a47cd697e968c76182d3d6bf879
Ulric Wilfred <shaman.sir@gmail.com>
2024-04-19 23:26:06 +0200

    actually rendering `Combo`s

--- src/App/Component/Combo.purs
+++ src/App/Component/Combo.purs
@@ -2,9 +2,17 @@ module App.Component.Combo where
 
 import Prelude
 
-import App.Keys (Key(..), Combo(..))
+import Data.Array.NonEmpty as NEA
+import Data.Traversable (traverse)
+import Data.Newtype (wrap) as NT
+import Data.String as String
+import Data.String.CodeUnits (singleton) as String
 
+import App.Keys (Key(..), Modifier(..), Combo(..))
+
+import Web.HTML.Common (ClassName)
 import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
 
 
 data State
@@ -12,17 +20,52 @@ data State
     | WaitingSecondKey
 
 
+cn :: String -> ClassName
+cn = NT.wrap
+
+
 render :: forall w i. Combo -> HH.HTML w i
-render =
-    case _ of
-        Combo keys def -> HH.span [ ] [ HH.text def ]
-        ModCombo mod combo -> HH.span [ ] [ HH.text "mod" ]
-        Sequence comboA comboB -> HH.span [] [ HH.text "seq" ]
+render c =
+    HH.div
+        [ HP.class_ $ cn "combo" ]
+        $ case c of
+            Combo keys def ->
+                [ HH.div
+                    [ HP.class_ $ cn "keys" ]
+                    $ key <$> NEA.toArray keys
+                , HH.div
+                    [ HP.class_ $ cn "def" ]
+                    [ HH.text def ]
+                ]
+            ModCombo mod combo ->
+                [ modifier mod
+                , render combo
+                ]
+            Sequence comboA comboB ->
+                [ render comboA
+                , HH.span_ [ HH.text "..." ]
+                , render comboB
+                ]
 
 
 key :: forall w i. Key -> HH.HTML w i
-key =
-    case _ of
-        Alpha char -> HH.span_ [ HH.text "c" ]
-        Num num -> HH.span_ [ HH.text $ show num ]
-        Special n str -> HH.span_ [ HH.text $ show n ]
+key k =
+    HH.div
+        [ HP.class_ $ cn "key" ]
+        $ case k of
+            Alpha char -> [ HH.span_ [ HH.text $ String.singleton char ] ]
+            Num num -> [ HH.span_ [ HH.text $ show num ] ]
+            Special n str ->
+                [ HH.span_ [ HH.text $ show n ]
+                , HH.span_ [ HH.text str ]
+                ]
+
+
+modifier :: forall w i. Modifier -> HH.HTML w i
+modifier m =
+    HH.div
+        [ HP.class_ $ cn "mod" ]
+        $ case m of
+            Shift -> [ HH.span_ [ HH.text "Shift" ] ]
+            Option -> [ HH.span_ [ HH.text "Option" ] ]
+            Command -> [ HH.span_ [ HH.text "Command" ] ]
